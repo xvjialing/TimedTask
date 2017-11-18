9cc61b7ed4f98f69a7883bdd79e3a6d63c33a407
xvjialing <xvjialing@outlook.com>
2017-11-18 15:03:30 +0800

    commit

--- src/main/java/org/xvjialing/xjl/common/CommonUtils.java
+++ src/main/java/org/xvjialing/xjl/common/CommonUtils.java
@@ -4,5 +4,6 @@ public interface CommonUtils {
     String SOCKET_URL="39.106.46.145";
     int SOCKET_PORT=8234;
 
-    String REST_SERVICE_URL="http://45.77.159.109:32772";
+//    String REST_SERVICE_URL="http://45.77.159.109:32772";
+    String REST_SERVICE_URL="http://120.78.193.185:8080";
 }
